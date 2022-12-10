1b49b84f89a352b210ae2ad7d322a0be77edfa9f
DinhNhat <nguyendinhnhat206@gmail.com>
2022-12-09 23:26:21 -0500

    - Update livewire vendor js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,6 +112,7 @@
             </div>
         </main>
 
-        @livewireScripts
+        {{-- @livewireScripts --}}
+        <script src="{{ asset('vendor/livewire/livewire.js') }}" defer></script>
     </body>
 </html>
