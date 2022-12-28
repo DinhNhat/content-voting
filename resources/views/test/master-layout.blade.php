efa5e05836b7bb89ae3ca5c4d267db9446f99720
DinhNhat <nguyendinhnhat206@gmail.com>
2022-12-28 14:25:08 -0500

    - Diable app.js script

--- resources/views/test/master-layout.blade.php
+++ resources/views/test/master-layout.blade.php
@@ -15,7 +15,7 @@
     @livewireStyles
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+{{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
 </head>
 
 <body class="font-sans bg-gray-background text-gray-900">
