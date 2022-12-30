e1c58e4749176ad91fb1a0595d448667ef403ea6
DinhNhat <nguyendinhnhat206@gmail.com>
2022-12-29 23:49:58 -0500

    - Update validation error colors

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -1,14 +1,21 @@
 @props(['errors'])
 
 @if ($errors->any())
-    <div {{ $attributes }}>
-        <div class="font-medium text-beach">
+    <div
+        x-data="{ isVisible: true }"
+        x-init="setTimeout(() => {
+            isVisible = false
+        }, 5000)"
+        x-show.transition.duration.1000ms="isVisible"
+        {{ $attributes }}
+     >
+        <div class="font-medium text-red">
             {{ __('Whoops! Something went wrong.') }}
         </div>
 
         <ul class="mt-3 list-disc list-inside">
             @foreach ($errors->all() as $error)
-                <li class="text-beach">{{ $error }}</li>
+                <li class="text-red">{{ $error }}</li>
             @endforeach
         </ul>
     </div>
