c6803654bf675fca787c988aa7202cd5f829e6d8
DinhNhat <nguyendinhnhat206@gmail.com>
2023-01-23 22:40:02 -0500

    -- Updated favicon in app and guest layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon_package/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon_package/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon_package/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicon_package/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('favicon_package/safari-pinned-tab.svg') }}" color="#9ed774">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#dcd94b">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
